989400f97169d2dfe03d3608e127ff627bf25a58
My Name <iqramulhaq00@gmail.com>
2021-08-23 17:49:37 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/sbs/flutter_in_app_purchase_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sbs.flutter_in_app_purchase_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/consumable_store.dart
@@ -0,0 +1,20 @@
+class ConsumableStore {
+
+
+  static List<String> load(){
+
+    return ['test1234'];
+
+  }
+
+  static  save(String ID){
+
+
+
+  }
+
+  static consume(String ID){
+
+  }
+
+}
